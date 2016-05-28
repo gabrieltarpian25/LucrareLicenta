8dd9b292c2937b7ab55ef20b444c6e0f7f471e2f
GabrielTarpian <gabriel.tarpian93@gmail.com>
2016-05-28 17:20:54 +0300

    Add Telekom Bill
    Add Telekom Bill into database

--- src/DownloadBill.java
+++ src/DownloadBill.java
@@ -84,7 +84,12 @@ public class DownloadBill {
 		WebDriverWait wait;
 		WebDriver driver;
 		
+		// for Mac OS
 		File pathToBinary = new File("/Applications/Firefox.app/Contents/MacOS/firefox");
+		
+		// For Windows
+		//File pathToBinary = new File("/Applications/Firefox.app/Contents/MacOS/firefox");
+		
 		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
 		FirefoxProfile firefoxProfile = new FirefoxProfile();
 		
@@ -144,6 +149,65 @@ public class DownloadBill {
 		// wait until download button is enabled
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("download")));
 		driver.findElement(By.id("download")).click();
+	}
+	
+	// **************************************************************************************************** ORANGE
+	public static void downloadTelekom() throws AWTException {
+		WebDriverWait wait;
+		WebDriver driver;
+
+		File pathToBinary = new File("/Applications/Firefox.app/Contents/MacOS/firefox");
+		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+		FirefoxProfile firefoxProfile = new FirefoxProfile();
+		
+		// firefox profile preferences
+		firefoxProfile.setPreference("browser.download.folderList",2);
+		firefoxProfile.setPreference("browser.download.manager.showWhenStarting",false);
+		firefoxProfile.setPreference("browser.download.dir","./Facturi");
+		firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+				"application/pdf,application/x-pdf");
+		
+		driver = new FirefoxDriver(ffBinary, firefoxProfile);
+		// driver = new FirefoxDriver();
+
+		wait = new WebDriverWait(driver, 60);
+		driver.get("https://www.telekom.ro");
+		
+		String username = null  ;
+		String password = null ;
+		
+		BufferedReader br = null;
+
+		try {
+
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader("./Details/loginTelekom.txt"));
+			//user name
+			sCurrentLine = br.readLine();
+			username = sCurrentLine;
+			
+			//password
+			sCurrentLine = br.readLine();
+			password = sCurrentLine;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		System.out.println();
+		System.out.println("Telekom Login details: ");
+		System.out.println("Username: "+username);
+		System.out.println("Password: "+password);
+		
+		driver.get("https://my.telekom.ro/ssologin/ssologin.jsp?contextType=external&username=string&OverrideRetryLimit=0&contextValue=%2Foam&password=sercure_string&challenge_url=https%3A%2F%2Fmy.telekom.ro%2Fssologin%2Fssologin.jsp&request_id=3671328571729533042&authn_try_count=0&locale=en_US&resource_url=https%253A%252F%252Fmy.telekom.ro%253A443%252Fms_oauth%252Foauth2%252Fui%252Foauthservice%252Fshowconsent%253Fresponse_type%25253Dcode%252526client_id%25253D7a1b4cd678874b0e90b52aeb558be4b8%252526redirect_uri%25253Dhttps%25253A%25252F%25252Fwww.telekom.ro%25252Fsso%25252Fsession%25253FfromNavMenu%25253Dtrue%252526scope%25253DUserProfileATG.me%252526oracle_client_name%25253DPOC");
+		driver.findElement(By.id("username")).sendKeys(new String[] { username });
+		driver.findElement(By.id("password")).sendKeys(new String[] { password });
+		driver.findElement(By.id("authSubmit")).click();
 	}
 
 }
--- src/TextParser.java
+++ src/TextParser.java
@@ -12,148 +12,201 @@ import org.apache.pdfbox.util.PDFTextStripper;
 public class TextParser {
 
 	
-// ***************************************************** Orange ***********************************************************************
-		public static void extractTextOrange(String filePath)
-		{
-			PDFTextStripper pdfStripper=null;
-			PDDocument pdDoc=null;
-			COSDocument cosDoc=null;
-			File file=new File(filePath);
-			
-			try
-			{
-				PDFParser parser=new PDFParser(new FileInputStream(file));
-				parser.parse();
-				cosDoc=parser.getDocument();
-				pdfStripper=new PDFTextStripper();
-				pdDoc=new PDDocument(cosDoc);
-				pdfStripper.setStartPage(1);
-				pdfStripper.setEndPage(2);
-				String parsedText=pdfStripper.getText(pdDoc);
-				System.out.println(parsedText);
-				
-				String lines[] = parsedText.split("\\r?\\n");
-				//System.out.println("Lines[3] =" + lines[3]);
-				
-				String numarFactura = null;
-				String dataFactura = null;
-				String dataScadenta = null;
-				String totalPlata = null;
-				
-				// parse every line
-				for(int i=0;i<lines.length;i++)
-				{
-					// Numar Factura
-					if(lines[i].contains("Numar factura: "))
-					{
-						numarFactura = lines[i].substring(15,lines[i].length()).trim();
-						System.out.println("Numar Factura: "+numarFactura);
-					}
-					
-					// Data Facturii
-					if(lines[i].contains("Data facturii: "))
-					{
-						dataFactura = lines[i].substring(15,lines[i].length()).trim();
-						System.out.println("Data Facturii: "+dataFactura);
-					}
-					
-					// Ultima zi de plata
-					if(lines[i].contains("Ultima zi de plata: "))
-					{
-						dataScadenta = lines[i].substring(20, lines[i].length()).trim();
-						System.out.println("Data scadenta: "+dataScadenta);
-					}
-					
-					// Total de plata
-					if(lines[i].contains("Factura curenta Orange"))
-					{
-						totalPlata = lines[i+3].trim();
-						System.out.println("Total Plata: "+totalPlata);
-					}
+// ***************************************************** ORANGE ***********************************************************************
+	public static void extractTextOrange(String filePath) {
+		PDFTextStripper pdfStripper = null;
+		PDDocument pdDoc = null;
+		COSDocument cosDoc = null;
+		File file = new File(filePath);
+
+		try {
+			PDFParser parser = new PDFParser(new FileInputStream(file));
+			parser.parse();
+			cosDoc = parser.getDocument();
+			pdfStripper = new PDFTextStripper();
+			pdDoc = new PDDocument(cosDoc);
+			pdfStripper.setStartPage(1);
+			pdfStripper.setEndPage(2);
+			String parsedText = pdfStripper.getText(pdDoc);
+			System.out.println(parsedText);
+
+			String lines[] = parsedText.split("\\r?\\n");
+			// System.out.println("Lines[3] =" + lines[3]);
+
+			String numarFactura = null;
+			String dataFactura = null;
+			String dataScadenta = null;
+			String totalPlata = null;
+
+			// parse every line
+			for (int i = 0; i < lines.length; i++) {
+				// Numar Factura
+				if (lines[i].contains("Numar factura: ")) {
+					numarFactura = lines[i].substring(15, lines[i].length()).trim();
+					System.out.println("Numar Factura: " + numarFactura);
 				}
-				
-				// convert totalPlata to float
-				totalPlata = totalPlata.replace(",",".");
-				float total = Float.parseFloat(totalPlata);
-				//System.out.println("Float total: "+total);
-				
-				DBUtil.insertIntoDatabase(numarFactura,"Orange",dataFactura,dataScadenta,total,"Neplatita",filePath);
-				//System.out.println(parsedText);
-			}catch(Exception e)
-			{
-				JOptionPane.showMessageDialog(null, "Eroare la citirea facturii Orange", "Eroare", JOptionPane.ERROR_MESSAGE);
-				e.printStackTrace();
+
+				// Data Facturii
+				if (lines[i].contains("Data facturii: ")) {
+					dataFactura = lines[i].substring(15, lines[i].length()).trim();
+					System.out.println("Data Facturii: " + dataFactura);
+				}
+
+				// Ultima zi de plata
+				if (lines[i].contains("Ultima zi de plata: ")) {
+					dataScadenta = lines[i].substring(20, lines[i].length()).trim();
+					System.out.println("Data scadenta: " + dataScadenta);
+				}
+
+				// Total de plata
+				if (lines[i].contains("Factura curenta Orange")) {
+					totalPlata = lines[i + 3].trim();
+					System.out.println("Total Plata: " + totalPlata);
+				}
 			}
+
+			// convert totalPlata to float
+			totalPlata = totalPlata.replace(",", ".");
+			float total = Float.parseFloat(totalPlata);
+			// System.out.println("Float total: "+total);
+
+			DBUtil.insertIntoDatabase(numarFactura, "Orange", dataFactura, dataScadenta, total, "Neplatita", filePath);
+			// System.out.println(parsedText);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Eroare la citirea facturii Orange", "Eroare",
+					JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
 		}
-//*****************************************************************************************************************************************
+	}
+//************************************************************************************************************************************
 		
-		// ***************************************************** Orange ***********************************************************************
-				public static void extractTextEon(String filePath)
-				{
-					PDFTextStripper pdfStripper=null;
-					PDDocument pdDoc=null;
-					COSDocument cosDoc=null;
-					File file=new File(filePath);
-					
-					try
-					{
-						PDFParser parser=new PDFParser(new FileInputStream(file));
-						parser.parse();
-						cosDoc=parser.getDocument();
-						pdfStripper=new PDFTextStripper();
-						pdDoc=new PDDocument(cosDoc);
-						pdfStripper.setStartPage(1);
-						pdfStripper.setEndPage(2);
-						String parsedText=pdfStripper.getText(pdDoc);
-						System.out.println(parsedText);
-						
-						
-						String lines[] = parsedText.split("\\r?\\n");
-						//System.out.println("Lines[3] =" + lines[3]);
-						
-						String numarFactura = null;
-						String dataFactura = null;
-						String dataScadenta = null;
-						String totalPlata = null;
-						
-						// parse every line
-						for(int i=0;i<lines.length;i++)
-						{
-							// Numar Factura , Data Facturii, Data Scadenta
-							if(lines[i].contains("Seria Numărul facturii Perioada de facturare Data emiterii Data scadenţei"))
-							{
-								String[] details = lines[i+1].split("EON");
-								String[] details2 = details[1].split(" ");
-								numarFactura = details2[1];
-								dataFactura = details2[2];
-								dataScadenta = details2[3];
-								
-								System.out.println("Numar Factura: "+numarFactura);
-								System.out.println("Data Emiterii: "+dataFactura);
-								System.out.println("Data Scadenta: "+dataScadenta);
-							}
-														
-							// Total de plata
-							if(lines[i].contains("Vă mulţumim!"))
-							{
-								totalPlata = lines[i-1].trim();
-								System.out.println("Total Plata (string): "+totalPlata);
-							}
-						}
-						
-						// convert totalPlata to float
-						totalPlata = totalPlata.replace(",",".");
-						float total = Float.parseFloat(totalPlata);
-						System.out.println("Total Plata (string): "+total);
-						
-						DBUtil.insertIntoDatabase(numarFactura,"E-ON",dataFactura,dataScadenta,total,"Neplatita",filePath);
-						//System.out.println(parsedText);
-					}catch(Exception e)
-					{
-						JOptionPane.showMessageDialog(null, "Eroare la citirea facturii E-ON", "Eroare", JOptionPane.ERROR_MESSAGE);
-						e.printStackTrace();
-					}
-					
+// ***************************************************** E-ON ***********************************************************************
+	public static void extractTextEon(String filePath) {
+		PDFTextStripper pdfStripper = null;
+		PDDocument pdDoc = null;
+		COSDocument cosDoc = null;
+		File file = new File(filePath);
+
+		try {
+			PDFParser parser = new PDFParser(new FileInputStream(file));
+			parser.parse();
+			cosDoc = parser.getDocument();
+			pdfStripper = new PDFTextStripper();
+			pdDoc = new PDDocument(cosDoc);
+			pdfStripper.setStartPage(1);
+			pdfStripper.setEndPage(2);
+			String parsedText = pdfStripper.getText(pdDoc);
+			System.out.println(parsedText);
+
+			String lines[] = parsedText.split("\\r?\\n");
+			// System.out.println("Lines[3] =" + lines[3]);
+
+			String numarFactura = null;
+			String dataFactura = null;
+			String dataScadenta = null;
+			String totalPlata = null;
+
+			// parse every line
+			for (int i = 0; i < lines.length; i++) {
+				// Numar Factura , Data Facturii, Data Scadenta
+				if (lines[i].contains("Seria Numărul facturii Perioada de facturare Data emiterii Data scadenţei")) {
+					String[] details = lines[i + 1].split("EON");
+					String[] details2 = details[1].split(" ");
+					numarFactura = details2[1];
+					dataFactura = details2[2];
+					dataScadenta = details2[3];
+
+					System.out.println("Numar Factura: " + numarFactura);
+					System.out.println("Data Emiterii: " + dataFactura);
+					System.out.println("Data Scadenta: " + dataScadenta);
 				}
-		//*****************************************************************************************************************************************
+
+				// Total de plata
+				if (lines[i].contains("Vă mulţumim!")) {
+					totalPlata = lines[i - 1].trim();
+					System.out.println("Total Plata (string): " + totalPlata);
+				}
+			}
+
+			// convert totalPlata to float
+			totalPlata = totalPlata.replace(",", ".");
+			float total = Float.parseFloat(totalPlata);
+			System.out.println("Total Plata (string): " + total);
+
+			DBUtil.insertIntoDatabase(numarFactura, "E-ON", dataFactura, dataScadenta, total, "Neplatita", filePath);
+			// System.out.println(parsedText);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Eroare la citirea facturii E-ON", "Eroare", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
+	}
+//*************************************************************************************************************************************
+	
+// ***************************************************** TELEKOM ***********************************************************************
+	public static void extractTextTelekom(String filePath) {
+		PDFTextStripper pdfStripper = null;
+		PDDocument pdDoc = null;
+		COSDocument cosDoc = null;
+		File file = new File(filePath);
+
+		try {
+			PDFParser parser = new PDFParser(new FileInputStream(file));
+			parser.parse();
+			cosDoc = parser.getDocument();
+			pdfStripper = new PDFTextStripper();
+			pdDoc = new PDDocument(cosDoc);
+			pdfStripper.setStartPage(1);
+			pdfStripper.setEndPage(2);
+			String parsedText = pdfStripper.getText(pdDoc);
+
+			String lines[] = parsedText.split("\\r?\\n");
+			// System.out.println("Lines[3] =" + lines[3]);
+
+			String numarFactura = null;
+			String dataFactura = null;
+			String dataScadenta = null;
+			String totalPlata = null;
+
+			// parse every line
+			for (int i = 0; i < lines.length; i++) 
+			{
+				System.out.println(lines[i]);
+				
+				// data factura
+				if(lines[i].contains("Data facturării: "))
+					dataFactura = lines[i].substring(17,lines[i].length());
+				// numar factura
+				if(lines[i].contains("Cod facturare: "))
+					numarFactura = lines[i].substring(15,lines[i].length());
+				
+				// total plata
+				if(lines[i].contains("Total factură curentă "))
+					totalPlata = lines[i].substring(22,lines[i].length());
+				
+				if(lines[i].contains("Total de plată până la "))
+					dataScadenta = lines[i].substring(23,33);
+			}
+
+			
+			// convert totalPlata to float
+			totalPlata = totalPlata.replace(",", ".");
+			float total = Float.parseFloat(totalPlata);
+			
+			System.out.println();
+			System.out.println("Detalii factura Telekom: ");
+			System.out.println("Numar factura: "+numarFactura+";");
+			System.out.println("Data factura: "+dataFactura+";");
+			System.out.println("Data scadenta: "+dataScadenta+";");
+			System.out.println("Total Plata (string): " + totalPlata+";");
+			System.out.println("Total Plata (float): " + total+";");
+
+			DBUtil.insertIntoDatabase(numarFactura, "Telekom", dataFactura, dataScadenta, total, "Neplatita", filePath);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Eroare la citirea facturii TELEKOM", "Eroare", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
+	}
+//*************************************************************************************************************************************
 }
